5dbcde35ee426a0ec159dc2159f17ea37867f3e9
Eduardo Maciel de Souza <edu.maciel@yahoo.com.br>
2020-11-11 00:00:03 -0300

    some updates

--- README.md
+++ README.md
@@ -16,18 +16,22 @@ A simple snake game created with the Pygame library.
 
 <details><summary><b> 3. How to run this game?</b></summary></br>
 
-1. You will need to have Python3 installed, that can be found [here](https://www.python.org/downloads).
-2. Download this repository to a folder of your choice.
-3. Open your folder using the terminal with this command (use the path to your folder):
+1. You will need to have Python3 installed, that can be found [here](https://www.python.org/downloads). Remember to select the installation of the pip manager too.
+2. Install the pygame library with this command in your terminal:
+    ```
+    pip install pygame
+    ```
+4. Download this repository to a folder of your choice.
+5. Open your folder using the terminal with this command (use the path to your folder):
     ```
     cd C:\Users\MyName\Documents\Myfolder\Snake-Game
     ```
-4. Run the application with Python3:
+6. Run the application with Python3:
     ```
     python Snake.py
     ```
-5. Click on "Start" to start the game.
-6. When the "GAME OVER" screen appears, press "Enter" to go back to the start screen.
+7. Click on "Start" to start the game.
+8. When the "GAME OVER" screen appears, press "Enter" to go back to the start screen.
 
 ---
 </details>
--- Snake.py
+++ Snake.py
@@ -61,24 +61,32 @@ def new_game():
         time.sleep(player.speed)
 
 def start_page():
+    print('Creating the Start Page...')
     pygame.mouse.set_visible(True)
     screen = pygame.display.get_surface()
     screen.fill((0, 0, 0))
+    print('     Loading Background...')
     background = Background('initial_background.png')
+    print('     Background Loaded...')   
     pygame.mouse.set_pos([400, 300])
+    print('     Loading Buttons...')   
     start_button = Button('start_off.png','start_on.png')
+    print('     Buttons Loaded...')   
     screen.blit(background.image, background.rect)
+    screen.blit(start_button.image_mouse_off, start_button.rect)
     pygame.display.flip()
 
+    print('Checking Inputs...')
     while 1:
         for event in pygame.event.get():
+            print(event.type)
             if event.type == pygame.QUIT:
                 sys.exit()
-            if event.type == 4:
+            if event.type == 1024:
                 if start_button.rect.collidepoint(pygame.mouse.get_pos()):
                     screen.blit(start_button.image_mouse_on, start_button.rect)
                 else: screen.blit(start_button.image_mouse_off, start_button.rect)
-            if event.type == 5 and start_button.rect.collidepoint(pygame.mouse.get_pos()):
+            if event.type == 1025 and start_button.rect.collidepoint(pygame.mouse.get_pos()):
                 new_game()
 
 
@@ -87,8 +95,10 @@ def start_page():
 
 def main():
 
+    print('Loading Pygame...')
     pygame.init()
     pygame.font.init()
+    print('Pygame loaded...')
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size, 0, 0)
     pygame.display.set_caption('SNAKE GAME')
